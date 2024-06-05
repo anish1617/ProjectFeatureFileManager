f4a04fe53f2b5ad64ec32159d723f506291f527e
Anish <basketb523@gmail.com>
2024-06-06 00:31:20 +0545

    add: docker test command added and test file setup successfully

--- ProjectFeatureFileManager/ProjectFeatureFileManager/obj/Debug/net8.0/ProjectFeatureFileManager.AssemblyInfo.cs
+++ ProjectFeatureFileManager/ProjectFeatureFileManager/obj/Debug/net8.0/ProjectFeatureFileManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectFeatureFileManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a4e44cff34d4fa2ed30cba34706a4bc9c19e3db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97ee421f8781de50c9a6fa4fc409ed1e66e90679")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectFeatureFileManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectFeatureFileManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
