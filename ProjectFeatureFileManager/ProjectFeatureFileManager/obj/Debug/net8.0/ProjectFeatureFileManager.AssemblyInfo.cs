1f9afeccd1746d08c69b362f84f1f7f004d312aa
Anish <basketb523@gmail.com>
2024-06-06 00:02:18 +0545

    Added debug folder changes (not recommended)

--- ProjectFeatureFileManager/ProjectFeatureFileManager/obj/Debug/net8.0/ProjectFeatureFileManager.AssemblyInfo.cs
+++ ProjectFeatureFileManager/ProjectFeatureFileManager/obj/Debug/net8.0/ProjectFeatureFileManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectFeatureFileManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3616b1f22dcc7e2a514513ecbd28d41cc7322809")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a4e44cff34d4fa2ed30cba34706a4bc9c19e3db")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectFeatureFileManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectFeatureFileManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
