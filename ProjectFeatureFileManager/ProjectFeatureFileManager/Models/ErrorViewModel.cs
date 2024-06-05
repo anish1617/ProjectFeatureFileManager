7eb119b0e97f58ade5d697585a9d596a9b66b24b
Anish <basketb523@gmail.com>
2024-06-05 23:18:48 +0545

    Added mvc projecr

---
+++ ProjectFeatureFileManager/ProjectFeatureFileManager/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProjectFeatureFileManager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
